eba54efb8f384d690d840117c0a6fb0b65162be3
Miguel Sanchez <msanchez.anaheim@gmail.com>
2017-12-14 17:15:38 -0800

    Added TodoList and TodoItem components

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TodoList from './TodoList';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        Hello World
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<TodoList/>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ client/src/TodoList.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import TodoItem from './TodoItem';
+
+const APIURL = '/api/todos'
+
+class TodoList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: []
+        }
+    }
+    
+    componentWillMount() {
+        this.loadTodos();
+    }
+
+    loadTodos() {
+        fetch(APIURL)
+            .then(resp => {
+                if(!resp.ok) {
+                    if(resp.status >= 400 && resp.status < 500) {
+                        return resp.json().then(data => {
+                            let err = {errorMessage: data.message};
+                            throw err;
+                        })
+                    } else {
+                        let err = {errorMessage: 'Try again later, server is not responding'};
+                        throw err;
+                    }
+                }
+                return resp.json();
+            })
+            .then(todos => this.setState({ todos }));
+    }
+
+    render() {
+        const todos = this.state.todos.map((t) => (
+            <TodoItem
+                key={t._id}
+                {...t}
+            />
+        ));
+
+        return (
+            <div>
+                <h1>Todo List App</h1>
+                <ul>{todos}</ul>
+            </div>
+        )
+    }
+}
+
+export default TodoList;
