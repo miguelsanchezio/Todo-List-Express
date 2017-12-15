8987c6cbfd45c66b94486c27881bcb095bba098f
Miguel Sanchez <msanchez.anaheim@gmail.com>
2017-12-15 00:02:11 -0800

    Added TodoForm component, users can now add, update, and delete list items

--- client/src/TodoItem.js
+++ client/src/TodoItem.js
@@ -1,7 +1,17 @@
 import React from 'react'
 
-const TodoItem = ({name}) => (
-    <li>{name}</li>
+const TodoItem = ({name, completed, _id, onDelete, onToggle}) => (
+    <li>
+        <span
+            style={{
+                textDecoration: completed ? 'line-through' : 'none'
+            }}
+            onClick={() => onToggle(_id, completed)}
+        >
+            {name}
+        </span>
+        <span onClick={() => onDelete(_id)}> X </span>
+    </li>
 )
 
 export default TodoItem;
--- client/src/TodoList.js
+++ client/src/TodoList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import TodoForm from './TodoForm';
 import TodoItem from './TodoItem';
 
 const APIURL = '/api/todos'
@@ -9,6 +10,9 @@ class TodoList extends Component {
         this.state = {
             todos: []
         }
+        this.addTodo = this.addTodo.bind(this);
+        this.deleteTodo = this.deleteTodo.bind(this);
+        this.toggleTodo = this.toggleTodo.bind(this);
     }
     
     componentWillMount() {
@@ -34,17 +38,105 @@ class TodoList extends Component {
             .then(todos => this.setState({ todos }));
     }
 
+    addTodo(val) {
+        fetch(APIURL, {
+            method: 'post',
+            headers: new Headers({
+                'Content-Type': 'application/json',
+            }),
+            body: JSON.stringify({ name: val })
+        })
+            .then(resp => {
+                if(!resp.ok) {
+                    if(resp.status >= 400 && resp.status < 500) {
+                        return resp.json().then(data => {
+                            let err = {errorMessage: data.message};
+                            throw err;
+                        })
+                    } else {
+                        let err = {errorMessage: 'Try again later, server is not responding'};
+                        throw err;
+                    }
+                }
+                return resp.json();
+            })
+            .then(newTodo => {
+                this.setState({ todos: [...this.state.todos, newTodo]} )
+            })
+    }
+
+    deleteTodo(id) {
+        const deleteURL = APIURL + '/' + id;
+        fetch(deleteURL, {
+            method: 'delete',
+        })
+            .then(resp => {
+                if(!resp.ok) {
+                    if(resp.status >= 400 && resp.status < 500) {
+                        return resp.json().then(data => {
+                            let err = {errorMessage: data.message};
+                            throw err;
+                        })
+                    } else {
+                        let err = {errorMessage: 'Try again later, server is not responding'};
+                        throw err;
+                    }
+                }
+                return resp.json();
+            })
+            .then(() => {
+                const todos = this.state.todos.filter(todo => todo._id !== id);
+                this.setState({ todos } )
+            })
+    }
+
+    toggleTodo(id, completed) {
+        const updateURL = APIURL + '/' + id;
+        fetch(updateURL, {
+            method: 'put',
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            }),
+            body: JSON.stringify({ completed: !completed })
+        })
+            .then(resp => {
+                if(!resp.ok) {
+                    if(resp.status >= 400 && resp.status < 500) {
+                        return resp.json().then(data => {
+                            let err = {errorMessage: data.message};
+                            throw err;
+                        })
+                    } else {
+                        let err = {errorMessage: 'Try again later, server is not responding'};
+                        throw err;
+                    }
+                }
+                return resp.json();
+            })
+            .then(updatedTodo => {
+                const todos = this.state.todos.map(todo =>
+                    (todo._id === updatedTodo._id)
+                    ? {...todo, completed: !todo.completed}
+                    : todo
+                    );
+                this.setState({ todos } );
+            });
+    }
+
     render() {
-        const todos = this.state.todos.map((t) => (
+        const todos = this.state.todos.map(t => (
             <TodoItem
                 key={t._id}
                 {...t}
+                onDelete={this.deleteTodo}
+                onToggle={this.toggleTodo}
             />
         ));
 
         return (
             <div>
                 <h1>Todo List App</h1>
+                <TodoForm addTodo={this.addTodo}/>
                 <ul>{todos}</ul>
             </div>
         )
